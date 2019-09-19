9909589ddcb5c98b36ede17899032442de2ddf55
Simon du Preez <s.dupreez@crimsoneducation.org>
2019-09-19 17:10:06 +1200

    WIIIIIP for leaving

---
+++ src/utils/typography.js
@@ -0,0 +1,15 @@
+
+import Typography from 'typography'
+import fontTheme from 'typography-theme-lincoln'
+
+const typography = new Typography(fontTheme)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
+export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
+
